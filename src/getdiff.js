42ecd5fa88278b8bda6bee1f2b4b26410fb85416
ysemenyuk <invent@list.ru>
2020-12-02 02:12:07 +0300

    step 8.3 (sort)

--- src/getdiff.js
+++ src/getdiff.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 
 const getDiff = (object1, object2) => {
   const resultPropertyNames = _.union(Object.keys(object1), Object.keys(object2));
-  return [...resultPropertyNames]
+  return resultPropertyNames
+    .slice()
     .sort()
     .map((name) => {
       if (!_.has(object2, name)) {
