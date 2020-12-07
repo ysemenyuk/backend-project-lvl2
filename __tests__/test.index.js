d6572e76d65d69d1c94fefd71bb3f035989118f0
ysemenyuk <invent@list.ru>
2020-12-07 23:37:40 +0300

    refactor step 1 (change test)

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -2,31 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
-test('gendiff', () => {
+test('gendiff "errors"', () => {
   expect(() => genDiff('__fixtures__/file1.json')).toThrow();
   expect(() => genDiff('__fixtures__/file1.json', 'file')).toThrow();
   expect(() => genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'fff')).toThrow();
 });
 
 test.each([
-  ['__fixtures__/file1.json', '__fixtures__/file2.json', 'expected_stylish.txt'],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.yml', 'expected_stylish.txt'],
-])('gendiff stylish "%s" "%s"', (a, b, name) => {
+  ['stylish', '__fixtures__/file1.json', '__fixtures__/file2.json', 'expected_stylish.txt'],
+  ['stylish', '__fixtures__/file1.yml', '__fixtures__/file2.yml', 'expected_stylish.txt'],
+  ['plain', '__fixtures__/file1.json', '__fixtures__/file2.json', 'expected_plain.txt'],
+  ['plain', '__fixtures__/file1.yml', '__fixtures__/file2.yml', 'expected_plain.txt'],
+  ['json', '__fixtures__/file1.json', '__fixtures__/file2.json', 'expected_json.json'],
+  ['json', '__fixtures__/file1.yml', '__fixtures__/file2.yml', 'expected_json.json'],
+])('gendiff "%s" "%s" "%s"', (f, a, b, e) => {
   const getFixturePath = (filename) => path.resolve('__fixtures__', filename);
-  const expected = fs.readFileSync(getFixturePath(name), 'utf-8');
-  expect(genDiff(a, b)).toEqual(expected);
-});
-
-test('gendiff plain', () => {
-  const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
-  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(expected);
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toEqual(expected);
-});
-
-test('gendiff json', () => {
-  const getFixturePath = (filename) => path.resolve('__fixtures__', filename);
-  const expected = fs.readFileSync(getFixturePath('expected_json.json'), 'utf-8');
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toEqual(expected);
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toEqual(expected);
+  const expected = fs.readFileSync(getFixturePath(e), 'utf-8');
+  expect(genDiff(a, b, f)).toEqual(expected);
 });
